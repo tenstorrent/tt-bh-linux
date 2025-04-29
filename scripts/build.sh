98e06c8dcf4c7865a05a4a0a5975adfbf4a006bf
Drew Fustini <dfustini@tenstorrent.com>
2025-04-29 00:00:04 +0000

    build.sh: switch from FW_PAYLOAD to FW_JUMP

--- scripts/build.sh
+++ scripts/build.sh
@@ -17,6 +17,7 @@ make CROSS_COMPILE=riscv64-linux-gnu- ARCH=riscv -j$(nproc)
 popd
 
 pushd $SBI
-CROSS_COMPILE=riscv64-linux-gnu- make FW_PIC=y FW_PAYLOAD_OFFSET=0x200000 PLATFORM=generic FW_PAYLOAD=y FW_PAYLOAD_PATH=$LINUX/arch/riscv/boot/Image FW_FDT_PATH=$DTB
+#CROSS_COMPILE=riscv64-linux-gnu- make FW_PIC=y FW_PAYLOAD_OFFSET=0x200000 PLATFORM=generic FW_PAYLOAD=y FW_PAYLOAD_PATH=$LINUX/arch/riscv/boot/Image FW_FDT_PATH=$DTB
+CROSS_COMPILE=riscv64-linux-gnu- make FW_PIC=y FW_JUMP=y FW_JUMP_OFFSET=0x200000 FW_JUMP_FDT_OFFSET=0x100000  PLATFORM=generic
 popd
 
