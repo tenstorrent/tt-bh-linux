4e233cef2045191765cc79da6f48f69079d67a57
Drew Fustini <dfustini@tenstorrent.com>
2025-04-29 00:00:10 +0000

    run.sh: switch from FW_PAYLOAD to FW_JUMP

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,10 +3,13 @@
 # Adjusted for FS of size 1200 MB
      FS_ADDR="0x4000e5000000"
 PAYLOAD_ADDR="0x400030000000"
+ KERNEL_ADDR="0x400030200000"
+    DTB_ADDR="0x400030100000"
 
 L2CPU=0
-PAYLOAD=$HOME/opensbi/build/platform/generic/firmware/fw_payload.bin
+DTB=$PWD/x280.dtb
+KERNEL=$HOME/linux/arch/riscv/boot/Image
+PAYLOAD=$HOME/opensbi/build/platform/generic/firmware/fw_jump.bin
 FS=$HOME/rootfs.ext4
 
-tt-smi -r0
-python boot.py --boot --l2cpu $L2CPU --opensbi_bin $PAYLOAD --opensbi_dst $PAYLOAD_ADDR --rootfs_bin $FS --rootfs_dst $FS_ADDR
+python boot.py --boot --l2cpu $L2CPU --opensbi_bin $PAYLOAD --opensbi_dst $PAYLOAD_ADDR --rootfs_bin $FS --rootfs_dst $FS_ADDR --kernel_bin $KERNEL --kernel_dst $KERNEL_ADDR --dtb_bin $DTB --dtb_dst $DTB_ADDR
