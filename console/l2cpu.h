b4247cb422b7942dc5080a0b6b81f8c6a323f863
Anirudh Srinivasan <asrinivasan@tenstorrent.com>
2025-05-06 21:04:27 -0400

    Add a lightweight l2cpu wraper class

---
+++ console/l2cpu.h
@@ -0,0 +1,35 @@
+#include <memory>
+#include <vector>
+#include <map>
+
+#include "ioctl.h"
+#include "tlb.h"
+
+class L2CPU
+{
+    int fd;
+
+    int idx;
+    uint64_t starting_address, memory_size;
+
+    xy_t coordinates;
+
+public:
+    L2CPU(int idx);
+
+    uint64_t get_starting_address();
+    uint64_t get_memory_size();
+
+    xy_t get_coordinates();
+
+    std::unique_ptr<TlbWindow2M> get_persistent_2M_tlb_window(uint64_t addr);
+
+    void write32(uint64_t addr, uint32_t value);
+
+    uint32_t read32(uint64_t addr);
+
+    ~L2CPU() noexcept;
+
+};
+const extern std::map<int, xy_t> l2cpu_tile_mapping;
+const extern std::map<int, uint64_t> l2cpu_starting_address_mapping, l2cpu_memory_size_mapping;
