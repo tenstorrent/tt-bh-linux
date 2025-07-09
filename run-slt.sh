d83329270e32a064a56458e6a7108ea3ef478da4
Drew Fustini <dfustini@tenstorrent.com>
2025-07-09 11:44:41 -0700

    SLT: add script to run stress test on all L2CPU tiles

---
+++ run-slt.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -x
+
+while [ /bin/true ];
+do
+	for ((L2CPU=0; L2CPU<=1; L2CPU++))
+	do
+		LOG="l2cpu_$L2CPU.log"
+		echo "L2CPU $L2CPU: START"
+		L2CPU=$L2CPU make boot &> $LOG &
+		for ((i=1; i<=60; i++))
+		do
+			RESULT=$(grep success $LOG)
+			if [ "$RESULT" == "" ]; then
+				echo -n "."
+				sleep 1
+			else
+				echo -e "\nRESULT: $RESULT"
+				pkill tt-bh-linux
+				ps -ef |grep tt-bh-linux
+				echo complete
+				break
+			fi
+	
+	
+		done
+		echo "L2CPU $L2CPU: END"
+		sleep 1
+	done
+done
